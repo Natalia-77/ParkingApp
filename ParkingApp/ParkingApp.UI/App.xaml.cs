8e6612d730e207225cdd921a4e8be79ad13aef2a
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-10-19 21:15:24 +0300

    create MAUI app

---
+++ ParkingApp/ParkingApp.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingApp.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
