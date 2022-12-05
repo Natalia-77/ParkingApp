06a6c6531002435141bd5e3efb5d166d942ddb32
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-12-05 23:56:02 +0200

    add data value bindings for changing colours depends on value

--- ParkingApp/ParkingApp.UI/Model/ParkingPlaceModel.cs
+++ ParkingApp/ParkingApp.UI/Model/ParkingPlaceModel.cs
@@ -5,5 +5,12 @@
         public int PlaceNumber { get; init; }
         public bool IsOccupied { get; init; }
         public CarModel OccupiedBy { get; init; }
+        public Color Colores
+        {
+            get
+            {
+                return IsOccupied? Color.FromRgb(253, 174, 174): Color.FromRgb(132, 247, 114);
+            }
+        }
     }    
 }
